e80046e5a5a728be54f1ab507180177a0c3c2493
Bolotov Alexander <distalker1@gmail.com>
2022-01-17 13:52:50 +0300

    pom fix

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,22 @@
 package jm.task.core.jdbc;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+        userService.saveUser("Рома", "Клинтаков", (byte) 28);
+        userService.saveUser("Лиза", "Тюлюбаева", (byte) 26);
+        userService.saveUser("Иван", "Сиднев", (byte) 29);
+        userService.saveUser("Зоя", "Донченко", (byte) 55);
+        userService.removeUserById(2);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,32 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/users";
+    private static final String USERNAME = "root";
+    private static final String PASS = "root";
+
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+        Connection connection = null;
+        try {
+            Driver driver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(URL, USERNAME, PASS);
+            Statement statement = connection.createStatement();
+            System.out.println(connection.isClosed());
+            if (!connection.isClosed()) {
+                System.out.println("Соединение с БД установлено");
+            }
+            connection.isClosed();
+            if (!connection.isClosed()) {
+                System.out.println("Соединение c БД закрыто");
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Не удалось соедениться с БД");
+            ;
+        } // реализуйте настройку соеденения с БД
+        return connection;
+    }
 }
